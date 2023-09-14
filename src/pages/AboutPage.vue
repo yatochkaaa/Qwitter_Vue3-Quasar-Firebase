7ffc02985016d996367e4663a52cd6abc5d72df3
TVV <TVV@d.c>
2023-09-14 17:44:01 +0300

    about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam non
+        officia error ut perspiciatis ea reprehenderit dicta earum sapiente eum,
+        voluptatum, dolor sed doloremque voluptatibus tenetur veritatis nulla
+        deleniti. Sunt!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam non
+        officia error ut perspiciatis ea reprehenderit dicta earum sapiente eum,
+        voluptatum, dolor sed doloremque voluptatibus tenetur veritatis nulla
+        deleniti. Sunt!
+      </p>
+    </div>
   </q-page>
 </template>
 
